db995447da8e5da6e41528eaaf43ee4c024ebebe
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-12-09 13:22:13 +0000

    feat: add migration to create `exercises` table

---
+++ infra/migrations/1733514105767_exercises.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("exercises", {
+    id: "id",
+    name: { type: "varchar(255)", notNull: true },
+    createdAt: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    reps: { type: "text" },
+    rest_seconds: { type: "integer" },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
